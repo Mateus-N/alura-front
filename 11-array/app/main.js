11dcfdc4913775fee37223382791dc5e57e62aff
Mateus Nunes <mateusnunes@frwk.com.br>
2023-05-03 12:11:37 -0300

    projeto array

---
+++ 11-array/app/main.js
@@ -0,0 +1,11 @@
+let livros = []
+const endpointDaApi = "https://guilhermeonrails.github.io/casadocodigo/livros.json"
+
+async function getBuscarLivrosDaApi () {
+  const res = await fetch(endpointDaApi)
+  livros = await res.json()
+  let livrosComDesconto = aplicaDesconto(livros)
+  exibirLivrosNaTela(livrosComDesconto)
+}
+
+getBuscarLivrosDaApi()
